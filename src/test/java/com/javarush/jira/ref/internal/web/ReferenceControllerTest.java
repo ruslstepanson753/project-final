de8ce6eac167602fc91990bb1ca6e56006ba1b95
user <ruslstepanson753@gmail.com>
2025-05-16 01:19:07 +0300

    task 5 done

--- src/test/java/com/javarush/jira/ref/internal/web/ReferenceControllerTest.java
+++ src/test/java/com/javarush/jira/ref/internal/web/ReferenceControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
