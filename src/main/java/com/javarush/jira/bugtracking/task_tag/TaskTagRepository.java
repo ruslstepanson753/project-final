ae87f1f35f187a8cefe0bc4d01df27c765a2f5a5
user <ruslstepanson753@gmail.com>
2025-05-18 17:48:13 +0300

    Task 7 get in swagger

--- src/main/java/com/javarush/jira/bugtracking/task_tag/TaskTagRepository.java
+++ src/main/java/com/javarush/jira/bugtracking/task_tag/TaskTagRepository.java
@@ -4,12 +4,13 @@ import com.javarush.jira.bugtracking.task.Activity;
 import com.javarush.jira.common.BaseRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
+@Repository
 public interface TaskTagRepository extends BaseRepository<TaskTag> {
     @Query("SELECT tt FROM TaskTag tt JOIN FETCH tt.task t JOIN FETCH t.project WHERE tt.id = :id")
     Optional<TaskTag> findByIdWithTaskAndProject(@Param("id") Long id);
--- src/main/java/com/javarush/jira/bugtracking/task_tag/TaskTagService.java
+++ src/main/java/com/javarush/jira/bugtracking/task_tag/TaskTagService.java
@@ -1,18 +1,23 @@
 package com.javarush.jira.bugtracking.task_tag;
 
-import com.javarush.jira.bugtracking.task.to.TaskToFull;
 import com.javarush.jira.bugtracking.task_tag.mapper.TaskTagMapper;
 import com.javarush.jira.bugtracking.task_tag.to.TaskTagTo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 public class TaskTagService {
 
-    TaskTagRepository taskTagRepository;
-    TaskTagMapper taskTagMapper;
+    private final TaskTagRepository taskTagRepository;
+    private final TaskTagMapper taskTagMapper;
 
+    public TaskTagService(TaskTagRepository taskTagRepository, TaskTagMapper taskTagMapper) {
+        this.taskTagRepository = taskTagRepository;
+        this.taskTagMapper = taskTagMapper;
+    }
+
+    @Transactional
     public TaskTagTo get(long id) {
         TaskTag taskTag = taskTagRepository.findByIdWithTaskAndProject(id).orElseThrow();
         TaskTagTo taskTagTo = taskTagMapper.toTo(taskTag);
